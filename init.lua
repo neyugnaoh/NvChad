a8e37b5130837edc43673e3dc0bb26b859717cfb
nvhoang2803 <nvhg2803@gmail.com>
2025-07-01 10:04:24 +0700

    update nvim configuration

--- init.lua
+++ init.lua
@@ -13,14 +13,14 @@ vim.opt.rtp:prepend(lazypath)
 -- Lazy.nvim configuration
 local lazy_config = require "configs.lazy"
 require("lazy").setup({
-    {
-      "NvChad/NvChad",
-       lazy = true,
-       branch = "v2.5",
-       import = "nvchad.plugins", config = function()
-         require "options"
-       end,
-     },
+  {
+    "NvChad/NvChad",
+    lazy = true,
+    branch = "v2.5",
+    import = "nvchad.plugins", config = function()
+      require "options"
+    end,
+  },
   { import = "plugins" },
   {
     'lewis6991/gitsigns.nvim',
@@ -155,26 +155,26 @@ require("lazy").setup({
     end,
   },
   { "typicode/bg.nvim", lazy = false },
--- {
---   "sainnhe/gruvbox-material",
---   priority = 1000, -- Ensure it loads before anything else
---   config = function()
---     vim.g.gruvbox_material_background = "hard" -- Options: 'soft', 'medium', 'hard'
---     vim.g.gruvbox_material_enable_italic = 1
---     vim.cmd("colorscheme gruvbox-material")
---   end
--- },
-{
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    bigfile = { enabled = true },
-    dashboard = {
+  -- {
+  --   "sainnhe/gruvbox-material",
+  --   priority = 1000, -- Ensure it loads before anything else
+  --   config = function()
+  --     vim.g.gruvbox_material_background = "hard" -- Options: 'soft', 'medium', 'hard'
+  --     vim.g.gruvbox_material_enable_italic = 1
+  --     vim.cmd("colorscheme gruvbox-material")
+  --   end
+  -- },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = {
         enabled = true,
         sections = {
           {
@@ -190,83 +190,82 @@ require("lazy").setup({
           },
         },
       },
-    explorer = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = false},
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
+      explorer = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = false},
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
   },
-},
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {
-      -- add any opts here
-      -- for example
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- timeout in milliseconds
-        temperature = 0, -- adjust if needed
-        max_tokens = 4096,
-        -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-      {
-        "nvzone/typr",
-        dependencies = "nvzone/volt",
-        opts = {},
-        cmd = { "Typr", "TyprStats" },
-      },
-    },
-  }
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  --   opts = {
+  --     -- add any opts here
+  --     -- for example
+  --     provider = "openai",
+  --     openai = {
+  --       endpoint = "https://api.openai.com/v1",
+  --       model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
+  --       timeout = 30000, -- timeout in milliseconds
+  --       max_tokens = 4096,
+  --       -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "echasnovski/mini.pick", -- for file_selector provider mini.pick
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+  --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --     {
+  --       "nvzone/typr",
+  --       dependencies = "nvzone/volt",
+  --       opts = {},
+  --       cmd = { "Typr", "TyprStats" },
+  --     },
+  --   },
+  -- }
 }, lazy_config)
 
 -- Load theme and statusline dofile(vim.g.base46_cache .. "defaults")
@@ -286,7 +285,7 @@ require('configs.gen');
 require('configs.lualine');
 require('configs.pomo');
 require('configs.pets');
-require('configs.avante')
+-- require('configs.avante')
 require('configs.noice');
 require('configs.harpoon');
 require('configs.rest');
